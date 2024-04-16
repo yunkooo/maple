250300f2f5761f253afb0a097d69f7bddee31a2a
yunkooo <yunkoooooo@gmail.com>
2024-04-16 17:04:15 +0900

    chore: mui ui -> tailwind css 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import IndexPage from './pages/IndexPage'
 
 function App() {
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div>React App</div>
+      <IndexPage />
     </QueryClientProvider>
   )
 }
