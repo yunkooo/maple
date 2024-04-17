e3f8b944866d06cb1dd36d1368bb60eba54e94eb
yunkooo <yunkoooooo@gmail.com>
2024-04-17 12:38:11 +0900

    fix: 1일 전 날짜 출력 / 표기 오류 수정

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,10 +1,11 @@
 const getDate = () => {
   const nowDate = new Date()
-  const month =
-    nowDate.getMonth() + 1 < 10
-      ? '0' + (nowDate.getMonth() + 1).toString()
-      : nowDate.getMonth() + 1
+  const prevDate = new Date(nowDate.setDate(nowDate.getDate() - 1))
 
-  return `${nowDate.getFullYear()}-${month}-${nowDate.getDate() - 1}`
+  const year = prevDate.getFullYear()
+  const month = (nowDate.getMonth() + 1).toString().padStart(2, '0')
+  const day = nowDate.getDate().toString().padStart(2, '0')
+
+  return `${year}-${month}-${day}`
 }
 export { getDate }
