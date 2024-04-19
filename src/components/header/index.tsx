9e72cfe9501cb900bb5cee2e594456c86f2b87f5
yunkooo <yunkoooooo@gmail.com>
2024-04-19 20:59:11 +0900

    refactor: 데이터를 받아오는 로직 삭제 및 변경

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,47 +1,39 @@
 import Moon from '@/asset/moon_icon.svg'
 import glass from '@/asset/magnifying_glass_icon.svg'
-import { useEffect, useState } from 'react'
-import { getChar, getOcid } from '@/api/charactor'
-import { getDate } from '@/utils/getDate'
-import useOcidStore from '@/store/store'
+import { useRef } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Header() {
-  const [charName, setCharName] = useState<string>('')
-  const [ocid, setOcid] = useState<string>('')
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  const setOcidData = useOcidStore(state => state.setOcidData)
-
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setCharName(event.currentTarget.value)
-  }
+  const navigate = useNavigate()
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const { ocid }: any = await getOcid(charName)
-    const info: any = await getChar(ocid, getDate())
-
-    setOcid(ocid)
+    if (inputRef.current) {
+      const nickName = inputRef.current.value
+      navigate(`/${nickName}`)
+    }
   }
-
-  useEffect(() => {
-    setOcidData(ocid)
-  }, [ocid])
 
   return (
     <header className="flex justify-center items-center gap-x-4 py-4">
-      <p className="text-2xl font-bold">MAPLE</p>
+      <Link
+        className="text-2xl font-bold"
+        to="/">
+        MAPLE
+      </Link>
       <form
         className="flex-1 flex border-2 px-2 rounded shadow-md"
         onSubmit={handleSubmit}>
         <input
           className="outline-none flex-1"
-          onChange={handleChange}
           type="text"
-          value={charName}
+          ref={inputRef}
           placeholder="닉네임"
         />
         <button
-          className="p-2"
+          className="w-4"
           type="submit">
           <img
             className="h-5"
