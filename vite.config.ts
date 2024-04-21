ccff70dfc8ba2fbe675760065d43624d6e92e189
yunkooo <yunkoooooo@gmail.com>
2024-04-22 00:50:23 +0900

    chore: vite-plugin-svgr 패키지 추가 및 구성

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
