0a6a46285b1491d4c84919bd71f482fdf6245bf8
yunkooo <yunkoooooo@gmail.com>
2024-04-21 18:35:08 +0900

    refactor: ocid 로직 수정 및 분리

--- src/components/characterInfo/index.tsx
+++ src/components/characterInfo/index.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react'
 import BasicInfo from '../basicInfo'
+import { getOcid } from '@/api/character'
+import { useParams } from 'react-router-dom'
+
+type OcidType = {
+  ocid: string | undefined
+}
 
 export default function CharacterInfo() {
+  const { nickname } = useParams()
+
+  const [ocidData, setOcidData] = useState<OcidType>({ ocid: '' })
+
+  const fetchOcidData = async (nickName: string | undefined) => {
+    if (nickName) {
+      const ocidResponse = await getOcid(nickName)
+      setOcidData(ocidResponse)
+    }
+  }
+  useEffect(() => {
+    fetchOcidData(nickname)
+  }, [nickname])
+
   return (
     <div>
-      <BasicInfo></BasicInfo>
+      <BasicInfo
+        nickName={nickname}
+        ocid={ocidData.ocid}></BasicInfo>
     </div>
   )
 }
